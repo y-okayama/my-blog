6f3fa36ed1cfdad9c0968ae43439beb7b9b641fc
yoshihiko okayama <yokayama0817@yahoo.co.jp>
2025-12-24 01:37:18 +0900

    debug: hello vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import Link from "next/link";
-import { getAllPosts } from "../lib/posts";
-
 export default function Home() {
-  const posts = getAllPosts();
-
-  return (
-    <main style={{ padding: "2rem", maxWidth: 800, margin: "0 auto" }}>
-      <h1>雑記ブログ</h1>
-
-      <ul style={{ padding: 0, listStyle: "none" }}>
-        {posts.map((post) => (
-          <li key={post.slug} style={{ marginBottom: "1.5rem" }}>
-            <Link href={`/posts/${post.slug}`}>
-              <strong style={{ fontSize: "1.2rem" }}>{post.title}</strong>
-            </Link>
-            <div style={{ opacity: 0.7, marginTop: "0.25rem" }}>
-              {post.date}
-            </div>
-            <p style={{ marginTop: "0.5rem" }}>{post.description}</p>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+  return <h1>HELLO VERCEL</h1>;
 }
